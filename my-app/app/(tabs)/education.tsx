f32485d59a8b2e1e87a82267dd4302b15aebc23f
golddog36 <goldgoldendog36@gamail.com>
2026-02-20 19:16:21 -0500

    trying out new asset paths

--- my-app/app/(tabs)/education.tsx
+++ my-app/app/(tabs)/education.tsx
@@ -26,8 +26,21 @@ export default function TabTwoScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedText>
-        This is the tab that explains what I've done for classes and how that has prepared me for internships and jobs
-        in the future.
+        I have received education(in the most robotic of terms) from Communion of Saints; Saint Ann(K-2nd grade),
+        Campus International School(3-8th grade), Campus International High School(9-12th grade), and most importantly
+        Case Western Reserve University(2024 - Present). However, I am also a very intrinsically motivated person and have
+        sought out resources and ways to learn on my own throughout my whole life.
+      </ThemedText>
+      <ThemedText>
+        I have performed very well academically here at CWRU, making Dean’s High Honors in two semesters. I have also
+        taken incredible courses here taught by some truly great people. My first semester here I took programming in Java
+        with Harold Connamacher which taught me how to look at assignments from the perspective of a computer scientist to
+        analyze and fully understand what I was doing, why, and how. In my second semester I took introduction to data
+        structure with Andrew Watkins which taught me many different things ranging from manipulation of data: searching
+        and sorting, hashing, recursion and higher order functions to abstract data types, templating, and the separation
+        of interface and implementation. In my third semester I took logic design, discrete math, and linear algebra with
+        Evren Cavusoglu, Shuai Xu, and Anuj Abhishek respectively and this is where I really started learning about how to
+        design for computer systems in a mathematical and precise way.
       </ThemedText>
     </ParallaxScrollView>
   );
