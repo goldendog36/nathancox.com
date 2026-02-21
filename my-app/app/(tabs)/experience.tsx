a9ada08f4b1e2c13018df439cb0557d0eff985c5
golddog36 <goldgoldendog36@gamail.com>
2026-02-20 20:46:42 -0500

    solve assets problem try 7

--- my-app/app/(tabs)/experience.tsx
+++ my-app/app/(tabs)/experience.tsx
@@ -26,8 +26,26 @@ export default function TabThreeScreen() {
         </ThemedText>
       </ThemedView>
       <ThemedText>
-        This is the tab that explains what I've done for work and how that has prepared me for internships and jobs
-        in the future.
+        While here at CWRU, I have also participated in the Fowler Center’s Business for World Benefit program where I
+        acted as a sustainability consultant from 2024 to the present. I’ve worked with companies from around the world
+        including Got Produce, Lock-In Billiards, RedHouse Studio and Rocket Arena to research sustainable solutions. I
+        demonstrated my professional business conduct working with companies alongside graduate students and at a time
+        acting as a consulting group leader. My work at the clinic has helped many businesses reach their goals for local
+        and global engagement and benefit.
+      </ThemedText>
+      <ThemedText>
+        In the summer between high school and college I started working for Beaumont, the boy scout camp my troop and I
+        went to when I was younger. There I acted as the handicraft director for the 2024 and 2025 summer seasons. I created
+        fourteen programs in my area to provide better memorable for the scouts participating. I worked well with others in
+        every position at the reservation to make many of the events on property happen and to improve the experience of
+        those who visited. I greatly contributed to our mission to prepare the scouts for life and provide fun experiences
+        for them.
+      </ThemedText>
+      <ThemedText>
+        I started working as soon as I could in high school at Nothing Bundt Cakes. I continued to work there frosting
+        cakes from April 2021 until I graduated high school in May 2024. I exceeded expectations in meeting all of Nothing
+        Bundt Cakes’s core values, working well with others, and putting an abundance of effort into the quality of every
+        order.
       </ThemedText>
     </ParallaxScrollView>
   );
